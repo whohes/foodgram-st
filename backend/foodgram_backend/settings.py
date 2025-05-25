521785b017f9a58f5eb7da0c7b2d89dce561e27f
whohes <dencelq_2007@mail.ru>
2025-05-25 22:12:15 +0600

    settings_chandges

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -112,9 +112,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'foodgram_db'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', 5432)
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'yourpassword'),
+        'HOST': os.getenv('DB_HOST', 'foodgram_db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
